47bc425bdde843e2faaa6510824f33f507c18e7c
Tobias <tobiasksu@gmail.com>
2021-03-24 14:50:50 -0500

    Initial WIP checkin

--- GameMod/PresetData.cs
+++ GameMod/PresetData.cs
@@ -7,7 +7,8 @@ using Harmony;
 using Overload;
 using UnityEngine;
 
-namespace GameMod {
+namespace GameMod
+{
     static class DataReader
     {
         public static string GetData(TextAsset ta, string filename)
@@ -24,11 +25,50 @@ namespace GameMod {
         }
         public static string GetProjData(TextAsset ta)
         {
-            return GetData(ta, "projdata.txt");
+            if (GameplayManager.LevelIsLoaded && GameplayManager.Level.IsAddOn)
+            {
+                Debug.Log("GetProjData 1");
+                string text3 = null;
+                string filepath = Path.Combine(Path.GetDirectoryName(GameplayManager.Level.FilePath), "projdata_" + GameplayManager.Level.Mission.FileName);
+                Debug.Log("Attempting to load custom projdata " + filepath);
+                byte[] array = Mission.LoadAddonData(GameplayManager.Level.ZipPath, filepath, ref text3, new string[]
+                {
+                ".txt"
+                });
+                if (array != null)
+                {
+                    return System.Text.Encoding.UTF8.GetString(array);
+                }
+                Debug.Log("Error loading custom projdata, reverting to stock");
+                return GetData(ta, "projdata.txt");
+            }
+            else
+            {
+                Debug.Log("GetProjData 2");
+                return GetData(ta, "projdata.txt");
+            }
         }
         public static string GetRobotData(TextAsset ta)
         {
-            return GetData(ta, "robotdata.txt");
+            if (GameplayManager.LevelIsLoaded && GameplayManager.Level.IsAddOn)
+            {
+                string text3 = null;
+                string filepath = Path.Combine(Path.GetDirectoryName(GameplayManager.Level.FilePath), "robotdata_" + GameplayManager.Level.Mission.FileName);
+                byte[] array = Mission.LoadAddonData(GameplayManager.Level.ZipPath, filepath, ref text3, new string[]
+                {
+                ".txt"
+                });
+                if (array != null)
+                {
+                    return System.Text.Encoding.UTF8.GetString(array);
+                }
+                Debug.Log("Error loading custom robotdata, reverting to stock");
+                return GetData(ta, "robotdata.txt");
+            }
+            else
+            {
+                return GetData(ta, "robotdata.txt");
+            }
         }
     }
 
@@ -57,6 +97,26 @@ namespace GameMod {
                     yield return new CodeInstruction(OpCodes.Call, dataReader_GetRobotData_Method);
                 else
                     yield return code;
+        }
+    }
+
+    [HarmonyPatch(typeof(GameplayManager), "OnSceneLoaded")]
+    class PresetData_GameplayManager_OnSceneLoaded
+    {
+        static void LoadCustomPresets()
+        {
+            RobotManager.Initialize();
+            ProjectileManager.ReadProjPresetData(ProjectileManager.proj_prefabs);
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            foreach (var code in codes)
+            {
+                if (code.opcode == OpCodes.Call && code.operand == AccessTools.Method(typeof(GameplayManager), "StartLevel"))
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PresetData_GameplayManager_OnSceneLoaded), "LoadCustomPresets"));
+                yield return code;
+            }
         }
     }
 }
