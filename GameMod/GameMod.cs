eeb84a0b4d582cb5dddbb2d07a9e0241631af465
Tobias <tobiasksu@gmail.com>
2021-08-07 20:10:17 -0500

    Remove uncapped menus for gsync support (shouldn't be needed w/ Doorstop)

--- GameMod/GameMod.cs
+++ GameMod/GameMod.cs
@@ -194,23 +194,23 @@ namespace GameMod.Core
         }
     }
 
-    // GSync fix
-    [HarmonyPatch(typeof(GameManager), "UpdateTargetFramerate")]
-    class GSyncFix
-    {
-        static bool Prefix()
-        {
-            if (GameplayManager.IsDedicatedServer())
-            {
-                Application.targetFrameRate = 120;
-            }
-            else
-            {
-                Application.targetFrameRate = -1;
-            }
-            return false;
-        }
-    }
+    //// GSync fix
+    //[HarmonyPatch(typeof(GameManager), "UpdateTargetFramerate")]
+    //class GSyncFix
+    //{
+    //    static bool Prefix()
+    //    {
+    //        if (GameplayManager.IsDedicatedServer())
+    //        {
+    //            Application.targetFrameRate = 120;
+    //        }
+    //        else
+    //        {
+    //            Application.targetFrameRate = -1;
+    //        }
+    //        return false;
+    //    }
+    //}
 
     // Shenanigans.
     [HarmonyPatch(typeof(StringParse), "IsNiceWord")]
